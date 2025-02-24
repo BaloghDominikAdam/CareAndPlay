5f671b8ff9cfcbef34bd139ecf5cd23075b97f9d
BaloghDominikAdam <dominikbalogh2000@gmail.com>
2025-02-24 23:52:17 +0100

    Auth ✅ 02.24
    ?
    game eloszto

--- public/assets/js/script.js
+++ public/assets/js/script.js
@@ -1,25 +1,3 @@
-// document.addEventListener('DOMContentLoaded', function() {
-//     const observer = new IntersectionObserver((entries) => {
-//         entries.forEach((entry) => {
-//             console.log(entry)
-//             if(entry.isIntersecting){
-//                 entry.target.classList.add('showc');
-//             } else{
-//                 entry.target.classList.remove('showc');
-//             }
-//         })
-
-//     })
-
-//     const hiddenElemets = document.querySelectorAll('.hiddenc');
-//     hiddenElemets.forEach((el) => observer.observe(el));
-
-
-
-
-// });
-
-
 document.addEventListener('DOMContentLoaded', function() {
 const navbar = document.getElementById("navnav");
 window.onscroll = (e) => {
@@ -28,5 +6,25 @@ window.onscroll = (e) => {
  } else {
  navbar.classList.remove("navbarbackground");
  }};
+
+
+ function confirm(ev){
+    ev.preventDefault();
+    var urlToDirect = ev.currentTarget.getAttribute('href');
+    console.log(urlToDirect);
+
+    swal({
+        title:"Gratulálok!",
+        text:"Sikeres bejelentkezés!",
+        icon: "success",
+        buttons: true,
+        dangerMode:false
+    })
+
+
+}
+
+
+
 });
 
